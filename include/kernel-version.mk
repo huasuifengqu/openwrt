b92cc8241dba0183a83502c41ee2eca9a25bd47a
John Audia <graysky@archlinux.us>
2020-09-17 09:51:25 -0400

    kernel: bump 5.4 to 5.4.66
    All modifications made by update_kernel.sh/no manual intervention needed
    
    Run-tested: ipq806x (R7800), ath79 (Archer C7v5), x86/64
    
    No dmesg regressions, everything appears functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    [add run test from PR]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -9,12 +9,12 @@ endif
 LINUX_VERSION-4.9 = .237
 LINUX_VERSION-4.14 = .199
 LINUX_VERSION-4.19 = .148
-LINUX_VERSION-5.4 = .65
+LINUX_VERSION-5.4 = .66
 
 LINUX_KERNEL_HASH-4.9.237 = 81ec5792c0ccdc244faff9dbb3d9c97857d9df4cb1f0c1c3db6bc4031faf861f
 LINUX_KERNEL_HASH-4.14.199 = 02f3432a262b3804745501f8c3c51d735133af48a39909df8b031d0b9653d4f9
 LINUX_KERNEL_HASH-4.19.148 = 3308c74a93ce6d4e495fde35d1351061240a895f76a288248c343713bd63555b
-LINUX_KERNEL_HASH-5.4.65 = f514834417d09de1667836e443e085bf37952603f23572b69ef0fcfda16cac69
+LINUX_KERNEL_HASH-5.4.66 = ff1930376774f4c1fc884d82184b5ebea6628f0a37ed9be781c0b119c4cfdab2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
